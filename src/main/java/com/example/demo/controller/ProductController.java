6d47d78c4ab1fed596b35aea67c28973d9909808
Aldous Roy <aldousroy007@gmail.com>
2026-04-25 14:30:36 +0530

    generated id created and the repo is created and the service also created to get all the products from the table

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,14 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
     @GetMapping()
     public String serverSetup(){
         return "Spring Boot Server Is Running";
+    }
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
