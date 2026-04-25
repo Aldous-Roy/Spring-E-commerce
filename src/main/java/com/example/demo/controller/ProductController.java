ae9ec574c204e887c916bacd67bed8a4ee9bd6d1
Aldous Roy <aldousroy007@gmail.com>
2026-04-25 14:14:58 +0530

    check and product model is created

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @GetMapping()
+    public String serverSetup(){
+        return "Spring Boot Server Is Running";
+    }
+}
